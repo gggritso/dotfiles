68984ce91590ce8e4f173355c93cb1a16de92151
George Gritsouk <george.gritsouk@mercatustechnologies.com>
2015-03-31 14:58:04 -0400

    Update fish prompt to include Git state.

--- fish/functions/fish_prompt.fish
+++ fish/functions/fish_prompt.fish
@@ -2,9 +2,11 @@ function fish_prompt
   z --add "$PWD"
 
   set_color $fish_color_cwd
-  echo -n (prompt_pwd)" "
-  echo -n "on "(hostname | cut -d . -f 1 )" "
+  echo -n (prompt_pwd)
+
+  set_color magenta
+  __fish_git_prompt
 
   set_color red
-  echo -n α" "
+  echo -n " "α" "
 end
