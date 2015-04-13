cf5921619490a014ab92c2e77652756bb6984239
George Gritsouk <george.gritsouk@mercatustechnologies.com>
2015-04-13 17:18:46 -0400

    Set nicer git status characters in prompt.

--- fish/config.fish
+++ fish/config.fish
@@ -1,1 +1,13 @@
-set -U PATH /usr/local/sbin $PATH
+set -x PATH $HOME/.rbenv/shims $HOME/.rbenv/bin /usr/local/sbin $PATH
+
+rbenv rehash >/dev/null ^&1
+
+set __fish_git_prompt_showdirtystate 'yes'
+set __fish_git_prompt_showupstream 'yes'
+set __fish_git_prompt_showuntrackedfiles 'yes'
+
+set __fish_git_prompt_char_dirtystate '∂'
+set __fish_git_prompt_char_upstream_prefix ''
+set __fish_git_prompt_char_upstream_ahead ' ↑'
+set __fish_git_prompt_char_upstream_behind ' ↓'
+set __fish_git_prompt_char_upstream_equal ''
