e27f87a2bd5f060825d1d0e6f4db04c19c0f8f10
George Gritsouk <george.gritsouk@gmail.com>
2017-11-23 20:38:00 -0500

    More vim, VIM FOREVER

---
+++ bootstrap.sh
@@ -0,0 +1,10 @@
+brew install fd
+brew install fzf
+brew install neovim
+
+mkdir ~/.config/nvim
+
+ln -s vimrc.symlink ~/.config/nvim/init.vim
+ln -s eslintrc ~/.eslintrc
+ln -s gitconfig.symlink ~/.gitconfig
+ln -s gitignore_global.symlink ~/.gitignore_global
