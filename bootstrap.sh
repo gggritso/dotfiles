59487e208292accf9c54b1a4a1d9563ef13e5974
George Gritsouk <george.gritsouk@gmail.com>
2017-12-16 23:31:28 -0500

    Update bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,9 @@
-brew install fd
-brew install fzf
 brew install neovim
-
 mkdir ~/.config/nvim
 
 ln -s vimrc.symlink ~/.config/nvim/init.vim
+
 ln -s eslintrc ~/.eslintrc
 ln -s gitconfig.symlink ~/.gitconfig
 ln -s gitignore_global.symlink ~/.gitignore_global
+ln -s VSCode ~/Library/Application\ Support/Code/User
