67872c6efd09debad28c9853c6e119cdfbb4b63c
George Gritsouk <george.gritsouk@gmail.com>
2019-02-18 21:25:14 -0500

    Add support for local.fish file

--- fish/config.fish
+++ fish/config.fish
@@ -1,7 +1,4 @@
 set -x EDITOR nvim
-set -x PATH  ~/.rbenv/shims $HOME/Code/dotfiles/bin $PATH
-
-thefuck --alias | source
 
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showupstream 'yes'
@@ -13,20 +10,9 @@ set __fish_git_prompt_char_upstream_ahead ' ↑'
 set __fish_git_prompt_char_upstream_behind ' ↓'
 set __fish_git_prompt_char_upstream_equal ''
 
-function __check_nvm --on-variable PWD --description 'Automatically nvm use'
-  if test -f .nvmrc
-    set node_version (nvm version)
-    set nvmrc_node_version (nvm version (cat .nvmrc))
-
-    if [ $nvmrc_node_version = "N/A" ]
-      nvm install
-    else if [ $nvmrc_node_version != $node_version ]
-      nvm use
-    end
-  end
-end
-
-__check_nvm
-
 set -gx NODE_ENV "development"
 set -gx NVM_DIR $HOME/.nvm
+
+if test -e ~/Code/dotfiles/fish/local.fish
+  source ~/Code/dotfiles/fish/local.fish
+end
