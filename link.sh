c6839a9c914e5db0d79b9e11925aa219eeb49082
George Gritsouk <989898+gggritso@users.noreply.github.com>
2022-03-10 09:52:38 -0500

    Update linker again

--- link.sh
+++ link.sh
@@ -1,6 +1,8 @@
-ln -sf ~/Code/Dotfiles/vimrc.symlink ~/.config/nvim/init.vim
-ln -sf ~/Code/Dotfiles/eslintrc.symlink ~/.eslintrc
-ln -sf ~/Code/Dotfiles/gitconfig.symlink ~/.gitconfig
-ln -sf ~/Code/Dotfiles/gitignore_global.symlink ~/.gitignore_global
-ln -sf ~/Code/Dotfiles/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -sf ~/Code/Dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
+ln -sf ~/Code/dotfiles/vimrc.symlink ~/.config/nvim/init.vim
+ln -sf ~/Code/dotfiles/eslintrc.symlink ~/.eslintrc
+ln -sf ~/Code/dotfiles/gitconfig.symlink ~/.gitconfig
+ln -sf ~/Code/dotfiles/gitignore_global.symlink ~/.gitignore_global
+ln -sf ~/Code/dotfiles/fish/config.fish ~/.config/fish/config.fish
+ln -sf ~/Code/dotfiles/fish/fish_plugins ~/.config/fish/fish_plugins
+ln -sf ~/Code/dotfiles/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
+ln -sf ~/Code/dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
