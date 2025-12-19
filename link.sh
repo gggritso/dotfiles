c9ef06d7a184afdda689281b78dbf99a122f7d0e
George Gritsouk <george@GTWRGCJ7HH.local>
2025-12-19 10:31:51 -0500

    Make a local fish config file

--- link.sh
+++ link.sh
@@ -3,3 +3,4 @@ ln -sf ~/Code/dotfiles/gitconfig.symlink ~/.gitconfig
 ln -sf ~/Code/dotfiles/gitignore_global.symlink ~/.gitignore_global
 ln -sf ~/Code/dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -sf ~/Code/dotfiles/fish/fish_plugins ~/.config/fish/fish_plugins
+touch ~/.config/fish/fish.local
