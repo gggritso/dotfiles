bd7f537aa0c2ddd7dfb074c6d730839aa4610ffb
George Gritsouk <george@GTWRGCJ7HH.local>
2025-12-19 10:33:04 -0500

    Remove Python and Ruby configs
    These should be local if they're even appropriate at all

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,6 @@
 set -gx EDITOR nvim
 
 eval (/opt/homebrew/bin/brew shellenv)
-status --is-interactive; and rbenv init - fish | source
-
-status is-login; and pyenv init --path | source
-status is-interactive; and pyenv init - | source
 
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showupstream 'yes'
