3b14aa2174d57f04ee6c57b34f8053dd3d7c68f3
George Gritsouk <george@GTWRGCJ7HH.local>
2025-12-19 10:34:44 -0500

    Add Zed configuration symlinks

--- link.sh
+++ link.sh
@@ -1,6 +1,8 @@
 ln -sf ~/Code/dotfiles/vimrc.symlink ~/.config/nvim/init.vim
 ln -sf ~/Code/dotfiles/gitconfig.symlink ~/.gitconfig
 ln -sf ~/Code/dotfiles/gitignore_global.symlink ~/.gitignore_global
+ln -sf ~/Code/dotfiles/zed/keymap.json ~/.config/zed/keymap.json
+ls -sf ~/Code/dotfiles/zed/settings.json ~/.config/zed/settings.json
 ln -sf ~/Code/dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -sf ~/Code/dotfiles/fish/fish_plugins ~/.config/fish/fish_plugins
 touch ~/.config/fish/fish.local
