31819f2c5c96d7027e3a7c2db0711a60448999aa
George Gritsouk <george@GTWRGCJ7HH.local>
2025-12-19 10:30:41 -0500

    Remove VS Code configs
    I don't really use it anymore

--- link.sh
+++ link.sh
@@ -3,5 +3,3 @@ ln -sf ~/Code/dotfiles/gitconfig.symlink ~/.gitconfig
 ln -sf ~/Code/dotfiles/gitignore_global.symlink ~/.gitignore_global
 ln -sf ~/Code/dotfiles/fish/config.fish ~/.config/fish/config.fish
 ln -sf ~/Code/dotfiles/fish/fish_plugins ~/.config/fish/fish_plugins
-ln -sf ~/Code/dotfiles/VSCode/keybindings.json ~/Library/Application\ Support/Code/User/keybindings.json
-ln -sf ~/Code/dotfiles/VSCode/settings.json ~/Library/Application\ Support/Code/User/settings.json
